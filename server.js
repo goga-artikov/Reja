2dde5f1c8a973ee13e63962198cf4201d4f41cdd
goga0917 <goga0917@gmail.com>
2026-08-26 21:13:51 +0900

    fear: develop author page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
-const http = require("http")
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -15,12 +26,16 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res){
-    res.end("<h1>HELLO WORLD</h1>");
+app.post("/create-item", (req, res) => {
+    // TODO code with db here
 });
 
 app.get("/gift", function (req, res){
     res.end("<h1>Siz endi sovg'alar bo'limidasiz</h1>");
+});
+
+app.get('/author', (req, res) => {
+  res.render("author", {user: user} )
 });
 
 
@@ -29,3 +44,6 @@ let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
+
+
+
