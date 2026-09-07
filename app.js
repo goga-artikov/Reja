a82ba93842acec54e92d6f22e8269c8b353906be
goga0917 <goga0917@gmail.com>
2026-09-07 23:01:13 +0900

    Reja loyihasi yakuniy bosqich kodlari yozib bo'lindi

--- app.js
+++ app.js
@@ -59,6 +59,22 @@ app.get("/", function (req, res) {
     });
 });
 
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate({ _id: new mongodb.ObjectId(data.id) }, { $set: { reja: data.new_input } }, function (err, data) {
+    res.json({ state: "succes" });
+  });
+});
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "hamma rejalar o'chirildi" });
+    });
+  }
+});
+
 app.get("/gift", function (req, res) {
   res.end("<h1>Siz endi sovg'alar bo'limidasiz</h1>");
 });
--- public/browser.js
+++ public/browser.js
@@ -11,7 +11,7 @@ function itemTemplate(item) {
       </div>
     </li>`;
 }
-console.log("**********")
+console.log("**********");
 let createField = document.getElementById("create-field");
 
 document.getElementById("create-form").addEventListener("submit", function (e) {
@@ -45,6 +45,25 @@ document.addEventListener("click", function (e) {
   }
   // edit oper
   if (e.target.classList.contains("edit-me")) {
-    alert("siz edit tugmasini bosdingiz");
+    let userInput = prompt("O'zgartirish kiriting", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+    if (userInput) {
+      axios
+        .post("/edit-item", { id: e.target.getAttribute("data-id"), new_input: userInput })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan harakat qiling!");
+        });
+    }
   }
 });
+
+document.getElementById("clean-all").addEventListener("click", function () {
+  axios.post("/delete-all", {delete_all: true}).then(response => {
+    console.log(response.data);
+    alert(response.data.state);
+    document.location.reload();
+  })
+})
